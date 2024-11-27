969f3bbd45e09863207350ff01a5e1046be74dc9
takaml <smt7151@gmail.com>
2024-11-27 10:11:24 +0300

    emad

--- lib/core/database/sqldb.dart
+++ lib/core/database/sqldb.dart
@@ -1,5 +1,7 @@
+
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import '../models/query.dart';
 
 
 
@@ -15,38 +17,32 @@ class SqlDb {
   }
 
   intialDb() async {
-    String databasepath = await getDatabasesPath();
-    String path = join(databasepath, "netcards.db");
-    print(path);
-    Database mydb = await openDatabase(path,
-        onCreate: _onCreate, version: 6, onUpgrade: _onUpgrade);
+    String path = join('assets', "database.db");
+      Database mydb = await openDatabase(path,
+        onCreate: _onCreate, version: 1, onUpgrade: _onUpgrade);
     return mydb;
   }
 
   _onUpgrade(Database db, int oldversion, int newversion) async {
-    print("onUpgrade ================");
+    // print("onUpgrade ================");
   }
 
   Future<void> _onCreate(Database db, int version) async {
     await db.execute('''
-  CREATE TABLE "users" (
-    "username" VARCHAR(15) NOT NULL UNIQUE ,
-    "comment" TEXT ,
-    "sn" TEXT ,
-    "profile" TEXT NOT NULL
-  )
+  CREATE TABLE "Tasks" (
+  "ID"  INTEGER,
+  "Name"  TEXT,
+  "Decription"  TEXT,
+  "TaskDate"  TEXT,
+  "CategoryID"  INTEGER,
+  "Priority"  TEXT CHECK("Priority" IN ('High', 'Medium', 'Low')),
+  "Status"  TEXT CHECK("Status" IN ('Pending', 'Expired', 'Postponed')),
+  "IsActive"  INTEGER CHECK("IsActive" IN (0, 1)),
+  "ImpDate"  TEXT,
+  PRIMARY KEY("ID" AUTOINCREMENT),
+  FOREIGN KEY("CategoryID") REFERENCES "Category"("ID")
+);
 ''');
-    await db.execute('''
-  CREATE TABLE "applinks" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT ,
-    "name" TEXT NOT NULL ,
-    "host" TEXT NOT NULL UNIQUE ,
-    "sshport" INTEGER NOT NULL,
-    "username" TEXT NOT NULL ,
-    "password" TEXT
-  )
-''');
-    print("On Create db=======");
   }
 
   Future<List<Map>> selectData(
@@ -56,7 +52,6 @@ class SqlDb {
     Query query = Query(table: table, where: where, customfields: customfields);
     Database? mydb = await db;
     List<Map> response = await mydb!.rawQuery(query.select());
-    // print(response);
     return Future.value(response);
   }
 
@@ -65,15 +60,12 @@ class SqlDb {
     Query query = Query(table: table);
     Database? mydb = await db;
     int response = await mydb!.rawInsert(query.insert(values: data));
-    // print(response);
     return Future.value(response);
   }
 
   Future<int> insertDataqq(String qq) async {
-    // Query query = Query(table: table);
     Database? mydb = await db;
     int response = await mydb!.rawInsert(qq);
-    // print(response);
     return Future.value(response);
   }
 
@@ -85,7 +77,6 @@ class SqlDb {
     Query query = Query(table: table, where: where);
     Database? mydb = await db;
     int response = await mydb!.rawUpdate(query.update(data: data));
-    // print(response);
     return Future.value(response);
   }
 
@@ -96,7 +87,6 @@ class SqlDb {
     Query query = Query(table: table, where: where);
     Database? mydb = await db;
     int response = await mydb!.rawDelete(query.delete());
-    // print(response);
     return Future.value(response);
   }
 }
